f02c822a33f39d1767c6ca26e6ace245b6806d1a
NicoGalis <ngalisteo.dam@institutcampalans.net>
2025-05-27 00:56:47 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ class Program
 {
     static void Main()
     {
-        System.Console.WriteLine(Holiii);
+        
+        System.Console.WriteLine("canvi desde el github");
         System.Console.WriteLine("hola");
         System.Console.WriteLine("canvi dos");
         System.Console.WriteLine("canvi per fer servir com a excusa per fer un commit 4");

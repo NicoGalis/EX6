631f83231b3035c746ec7c3ce727e073b733cfd4
Nico <ngalisteo.dam@institutcampalans.net>
2025-05-27 00:27:36 +0200

    he afegit holaaa

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("hola");
+        Console.Write("Introdueix un valor enter: ");
+        if (int.TryParse(Console.ReadLine(), out int limit))
+        {
+            List<int> multiples = Metodes.ComptadorMultiples(limit);
+            Console.WriteLine($"Els nombres múltiples de 7 menors que {limit} són: {string.Join(", ", multiples)}");
+        }
+        else
+        {
+            Console.WriteLine("Si us plau, introdueix un nombre enter vàlid.");
+        }
+    }
+}
+
+//holaaaa
+
+class Metodes
+{
+    public static List<int> ComptadorMultiples(int limit)
+    {
+        List<int> multiples = new List<int>();
+
+        for (int i = 1; i < limit; i++)
+        {
+            if (i % 7 == 0)
+            {
+                multiples.Add(i);
+            }
+        }
+
+        return multiples;
+    }
+}
+
